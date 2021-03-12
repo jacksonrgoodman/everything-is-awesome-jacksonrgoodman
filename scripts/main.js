9a6aeefa028f65bf97dc234118fb0837bb06405c
JacksonRGoodman <jacksonrgoodman@gmail.com>
2021-03-12 12:00:49 -0600

    filters and stuff

--- scripts/main.js
+++ scripts/main.js
@@ -5,12 +5,34 @@ import { makeLegoList } from './legos/LegoList.js';
 const navElement = document.querySelector("nav");
 
 navElement.addEventListener("click", (event) => {
-	if (event.target.id === "showBlue") {
-		filterLegos("Blue")
+	if (event.target.id === "showRed") {
+		filterLegos("Red")
 	} else if (event.target.id === "showAll") {
 		makeLegoList(useLegos())
 	}
 })
+navElement.addEventListener("click", (event) => {
+	if (event.target.id === "showGreen") {
+		filterLegos("Green")
+	} else if (event.target.id === "showAll") {
+		makeLegoList(useLegos())
+	}
+})
+
+//? helper function
+
+//? dropdown filter for material
+const materialElement = document.querySelector("#materialSelector")
+materialElement.addEventListener("change", (event) => {
+	if (event.target.id === "materialSelector") {
+		const materialValue = (event.target.value);
+		filterMaterials(materialValue);
+	}
+})
+//? what do i need to pass in to filter logs?? look through event object for
+
+//? LegoID search box with enter key
+
 
 const filterLegos = (whatFilter) => {
 	const filterArray = useLegos().filter(singleLego => {
@@ -20,7 +42,14 @@ const filterLegos = (whatFilter) => {
 	})
 	makeLegoList(filterArray);
 }
-
+const filterMaterials = (whatFilter) => {
+	const filterArray = useLegos().filter(singleLego => {
+		if (singleLego.Material.includes(whatFilter)) {
+			return singleLego;
+		}
+	})
+	makeLegoList(filterArray);
+}
 
 const startEIA = () => {
 	loadLegos()
